d24d4202512a109464e09429686862f7e41fc916
rankshow <uwaomaobinna20@gmail.com>
2023-02-12 11:57:13 +0100

    added evry image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { HeartFilled } from '@ant-design/icons';
+import Heart from '../components/images/heart.png';
+import InnerSymbol from '../components/images/hearts.png';
+import Archivebottom from '../components/images/bin.png';
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='NavHead'>
-      <div className='IconNav'>I <span><HeartFilled /></span> yalbase</div>
+      <div className='IconNav'>
+        I<span><img className='archive' src={Archivebottom} alt="Archive" /><img className='Heart' src={Heart} alt="love logo" /><img className='heartsymbol' src={InnerSymbol} alt="love symbol" /></span>yalbaze
+        </div>
       <div className='SearchNav'>
-        <button className='btnNav'>Get Priority Access</button>
+        <button className='NavBtn'>Get Priority Access</button>
       </div>
     </div>
   )
 }
+
 
 export default Navbar;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,35 +1,41 @@
 import React from "react";
 import './section.css';
+import Ellipse from '../components/images/Ellipse 39 (1).png';
 
 const Section = () => {
   return (
     <section>
+       <div className="ellipse">
+        <img  src={Ellipse} alt="Ellipse 39" />
+        </div>
       <div className="mainsection">
          <div className="subSection">
-           <h1>Turn your best customers</h1>
-          <h2>
-            into <span>Loyal fans</span>
-          </h2>
+           <h1>Turn your best customers <br />
+          into <span>Loyal fans</span></h1>
         </div>
-        {/* <div className="childSection"> */}
-        {/* </div> */}
         <div className="mainText">
         <p>
           Loyalbaze, empowers businesses to offers digital, mobile-first loyalty
           programs to their customers. With our AI powered platform, you can
           easily create and manage custom loyalty and rewards programs, track
-          customer engagement, and gain valuable insight to improve your
+          customer engagement, and gain valuable insight to improve your business. Sign up now to get early access.
         </p>
         </div>
       </div>
       <div className="formHead">
         <form>
+          <div className="name">
           <input type="text" placeholder='Tell us your name' />
+          </div>
+          <div className="email">
           <input type="text" placeholder='Enter your email address' />
-          <button className="btnSubmit">
+          </div>
+           <div className="btnSubmit">
+          <button>
             Get early access
           </button>
-
+          </div>
+            <p>+57 joined</p> 
         </form>
       </div>
     </section>
