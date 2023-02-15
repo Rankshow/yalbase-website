6f30387c1288566299f88d426831007de28a6264
rankshow <uwaomaobinna20@gmail.com>
2023-02-15 22:55:54 +0100

    finished first section

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,8 +4,14 @@ import Ellipse39 from '../components/images/Ellipse 39 (1).png';
 import Ellipse37 from '../components/images/Ellipse 37.png';
 import StarTwo from '../components/images/Star 2.png';
 import StarThree from '../components/images/Star 3.png';
+import StarFive from '../components/images/Star 4.png';
 import UserOutline from '../components/images/Vector@2x.png';
 import UserCircle from '../components/images/VectorName.png';
+import EllipseBlur from '../components/images/Ellipse 39.png';
+import EllipseBlue from '../components/images/Ellipse 33.png'
+import EllipsePink from '../components/images/Ellipse 33 (1).png'
+import Vector from '../components/images/Vector.png';
+import StarFour from '../components/images/Star 2.png';
 import {MailOutlined} from '@ant-design/icons'
 
 const Section = () => {
@@ -37,6 +43,9 @@ const Section = () => {
         Loyalbaze, empowers businesses to offer digital, mobile-first loyalty programs to their customers. With our AI Powered platform, you can easily create and manage custom loyalty and rewards programs, track customer engagement, and gain valuable insights to improve your business. Sign up now to get early access.
         </p>
       </div>
+      <div>
+        <img className="EllipseBlur" src={EllipseBlur} alt="" />
+      </div>
 
       {/* form */}
       <div className="formHead">
@@ -51,12 +60,41 @@ const Section = () => {
             <MailOutlined className="emailIcon" />
               <input type="text" placeholder='Enter your email address' />
           </div>
+          <div className="Vector">
+            <img src={Vector} alt="Arrow head" />
+          </div>
            <div className="btnSubmit">
+            {/*Get early button */}
           <button className="submit">
             Get early access
           </button>
           </div>
-            <p>+57 joined</p> 
+          {/* Last section */}
+          <div className="LastSection">
+            <div>
+              <img className="ellpse1" src={EllipseBlue} alt="ellipse33" />
+               </div>
+                <div>
+                  <span>G</span>
+                  <img className="ellpse2" src={EllipsePink} alt="ellipse33" />
+                   </div>
+                   <div>
+                    <span>O</span>
+                     <img className="ellpse3" src={EllipseBlue} alt="ellipse33" />
+                    </div>
+                    <div>
+                    <span>U</span>
+                    <img className="ellpse4" src={EllipsePink} alt="ellipse33" />
+                   <span>M</span>
+                   </div>
+                     <p>+57 joined</p>
+                   </div> 
+                 <div className="StarFour">
+              <img src={StarFour} alt="Star four" />
+            </div>   
+          <div className="StarFive">
+            <img src={StarFive} alt="Star five" />
+          </div>
         </form>
       </div>
     </section>
